a9b367d1b1af3010c74b774173961f0356fb490e
Félix <fcote@kth.se>
2021-12-29 19:03:54 +0100

    Changed server to OSC

--- PYTHON/misc/erosion_client.py
+++ PYTHON/misc/erosion_client.py
@@ -1,24 +1,18 @@
 import socket
+from pythonosc import udp_client
 
-hote = "192.168"
-port = 9000
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
-for i in range(0, 2) :
-    for j in range(0, 256) :
-        ip = hote + ".{}.{}".format(i, j)
-
-        socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.connect((ip, port))
-
-        try :
-            socket.send("hello")
-        except:
-            print("not connected to {}".format(ip))
-
-c = """
-
-print("Connection on {}".format(port))
-
-print("Close")
-socket.close()
-"""
+ipAdress = '.'.join(get_ip().split('.')[:-1] + ["255"])
+client = udp_client.SimpleUDPClient(ipAdress, 8000)
+client.send_message("/filter", 9999)
--- PYTHON/misc/erosion_server.py
+++ PYTHON/misc/erosion_server.py
@@ -1,17 +1,21 @@
+from pythonosc import dispatcher
+from pythonosc import osc_server
 import socket
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(('192.168.1.19', 9000))
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
-while True:
-        socket.listen(5)
-        client, address = socket.accept()
-        print("{} connected".format(address))
+dispatcher = dispatcher.Dispatcher()
+dispatcher.map("/filter", print)
 
-        response = client.recv(255)
-        if response != "":
-                print(response)
-
-print("Close")
-client.close()
-stock.close()
+server = osc_server.ThreadingOSCUDPServer((get_ip(), 8000), dispatcher)
+server.serve_forever()
