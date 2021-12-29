85b7e626606cc097aa9d44930a9ac6dca1552cad
Félix <fcote@kth.se>
2021-12-29 17:33:17 +0100

    Added client and server scripts.

---
+++ PYTHON/misc/erosion_client.py
@@ -0,0 +1,11 @@
+import socket
+
+hote = "localhost"
+port = 15555
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((hote, port))
+print("Connection on {}".format(port))
+
+print("Close")
+socket.close()
